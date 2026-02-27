1e5d457f99f10711c1cfeae17d23edeb2faa8b49
Alexsandro <alex.dds432@gmail.com>
2026-02-27 18:00:26 -0300

    6-add e estilizando section cards

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,14 @@
 // AQUI PROCURA OS ELEMENTOS NO HTML E COLOCA ELES NAS VARIAVEIS
-const btnfiltro = document.getElementById("btnfiltro");
+const containerfilter = document.querySelector(".containerfilter");
 const painelfiltro = document.getElementById("painelfiltro");
 
 // QUANDO ACONTECER O EVENTO DE CLICAR NO BOTAO, VAI EXECUTAR A FUNCAO
-btnfiltro.addEventListener("click", function() {
+containerfilter.addEventListener("mouseenter", function() {
   // ESSA FUNCAO DIZ QUE SE TIVER A CLASSE ACTIVE, REMOVE E SE NAO TIVER COLOCA
-  painelfiltro.classList.toggle("active");
+  painelfiltro.classList.add("active");
 });
 // QUANDO ACONTECER O EVENTO DE CLICAR EM QUALQUER LUGAR DA TELA, VAI EXECUTAR A FUNCAO
-document.addEventListener("click", function(event) {
-  // SE O CLIQUE NAO FOI DENTRO DO PAINEL E NAO FOI DENTRO DO BOTAO, ENTAO EXECUTE O QUE ESTA ABAIXO.
-  if (!painelfiltro.contains(event.target) && !btnfiltro.contains(event.target)) {
+containerfilter.addEventListener("mouseleave", function(event) {
     // REMOVE A CLASSE ACTIVE
     painelfiltro.classList.remove("active");
-}});
+});
