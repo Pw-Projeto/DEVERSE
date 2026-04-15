ecec01412606106b7a5ec686e67f84051a190feb
Alexsandro <alex.dds432@gmail.com>
2026-04-15 11:31:29 -0300

    finalizando para apresentacao

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,33 @@
-// AQUI PROCURA OS ELEMENTOS NO HTML E COLOCA ELES NAS VARIAVEIS
-const containerfilter = document.querySelector(".containerfilter");
-const painelfiltro = document.getElementById("painelfiltro");
+const filterContainer = document.querySelector(".filtro-wrapper");
+const filterBtn = document.getElementById("btnfiltro");
+const filterPanel = document.getElementById("painelfiltro");
+const pesquisaContainer = document.querySelector(".search-box");
+const pesquisaInput = document.querySelector(".search-input");
+const pesquisaBtn = document.querySelector(".buscar");
 
-// QUANDO ACONTECER O EVENTO DE PASSAR O MOUSE POR CIMA, VAI EXECUTAR A FUNCAO
-containerfilter.addEventListener("mouseenter", function() {
-  // ADICIONA A FUNCAO ACTIVE
-  painelfiltro.classList.add("active");
+
+// TOGGLE
+filterBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  filterPanel.classList.toggle("is-active");
 });
-// QUANDO ACONTECER O EVENTO DE TIRAR O MOUSE DE CIMA, VAI EXECUTAR A FUNCAO
-containerfilter.addEventListener("mouseleave", function() {
-    // REMOVE A CLASSE ACTIVE
-    painelfiltro.classList.remove("active");
+
+// NÃO FECHAR AO CLICAR DENTRO
+filterPanel.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
+// FECHAR AO CLICAR FORA
+document.addEventListener("click", function () {
+  filterPanel.classList.remove("is-active");
+});
+
+
+pesquisaBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  pesquisaInput.classList.toggle("is-active");
+});
+
+pesquisaInput.addEventListener("click", function (e) {
+  e.stopPropagation();
 });
